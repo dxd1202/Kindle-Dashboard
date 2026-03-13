1bb524472e742541a807b32363f8b5b52c3d19d0
dxd1202 <dxd1202@users.noreply.github.com>
2026-03-13 08:03:19 +0000

    deploy: 891f9866041dca761043df29cad1819030ee4dc2

---
+++ CMakeFiles/Kindle-Dashboard.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/runner/work/Kindle-Dashboard/Kindle-Dashboard/src/main.cpp" "CMakeFiles/Kindle-Dashboard.dir/src/main.cpp.o" "gcc" "CMakeFiles/Kindle-Dashboard.dir/src/main.cpp.o.d"
+  "/home/runner/work/Kindle-Dashboard/Kindle-Dashboard/src/renderer.cpp" "CMakeFiles/Kindle-Dashboard.dir/src/renderer.cpp.o" "gcc" "CMakeFiles/Kindle-Dashboard.dir/src/renderer.cpp.o.d"
+  "" "Kindle-Dashboard" "gcc" "CMakeFiles/Kindle-Dashboard.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Kindle-Dashboard.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kindle-Dashboard.dir/link.d"
+  "CMakeFiles/Kindle-Dashboard.dir/src/main.cpp.o"
+  "CMakeFiles/Kindle-Dashboard.dir/src/main.cpp.o.d"
+  "CMakeFiles/Kindle-Dashboard.dir/src/renderer.cpp.o"
+  "CMakeFiles/Kindle-Dashboard.dir/src/renderer.cpp.o.d"
+  "Kindle-Dashboard"
+  "Kindle-Dashboard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kindle-Dashboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
